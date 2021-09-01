8767c42acd90b2cdafe8e43d93f77df9334deba6
xiezy <xiezy@smartebao.com>
2021-09-01 16:36:29 +0800

    add plugin

---
+++ jenkins辅助插件/JenlinksAuxiliary/content-script.js
@@ -0,0 +1,15 @@
+﻿(function () {
+  console.log('这是 simple-chrome-plugin-demo 的content-script！');
+  var list = [...document.getElementsByClassName('form-group')];
+  if (!list.length) return;
+
+  try {
+    list.forEach(item => {
+      if (item.getElementsByClassName('setting-name')[0].innerText === 'branch') {
+        item.className += ' JenlinksAuxiliary';
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})();
