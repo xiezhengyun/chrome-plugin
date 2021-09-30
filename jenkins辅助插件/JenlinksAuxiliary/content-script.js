c3ed52231f8d421fc8b13a19a95baa1f3edd6811
xiezy <xiezy@smartebao.com>
2021-09-30 13:55:49 +0800

    fix: JenlinksAuxiliary

--- jenkins辅助插件/JenlinksAuxiliary/content-script.js
+++ jenkins辅助插件/JenlinksAuxiliary/content-script.js
@@ -1,14 +1,15 @@
 ﻿(function () {
   console.log('这是 simple-chrome-plugin-demo 的content-script！');
-  var list = [...document.getElementsByClassName('form-group')];
+
+  var list = [...document.getElementsByClassName('setting-name')];
   if (!list.length) return;
 
   try {
-    list.forEach(item => {
-      if (item.getElementsByClassName('setting-name')[0].innerText === 'branch') {
-        item.className += ' JenlinksAuxiliary';
+    for (let i = 0; i < list.length; i++) {
+      if (list[i].innerText === 'branch') {
+        list[i].parentNode.className += ' JenlinksAuxiliary';
       }
-    });
+    }
   } catch (error) {
     console.log(error);
   }
